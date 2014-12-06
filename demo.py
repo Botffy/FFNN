d92c3a65ac12cff9043ffbed05e92085345e10eb
Ármin Scipiades <scipiades.armin@gmail.com>
2014-12-06 01:24:29 +0100

    Make demo slightly saner

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import math
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -8,35 +9,43 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import ffnn
 
 
-def func(x, y):
-	return (math.cos(2.5*math.pi*x) + math.sin(1.5*math.pi*y)) * (x+y)
+def plot3d(func, domain, gridsize, plotres=50):
+	print "Creating FFNN..."
+	t0 = time.clock()
+	approximator = ffnn.blumli(func, gridsize, domain)
+	print "...done ({} sec)".format(time.clock()-t0)
 
-domain = {
-	'x': (-1, 1),
-	'y': (-1, 1)
-}
+	t0 = time.clock()
+	print "Calculating {} values...".format(plotres**2)
 
-# ask Blum-Li to creat an FFNN for func:
-approximator = ffnn.blumli(func, 20, (domain['x'], domain['y']))
+	x = np.linspace(domain[0][0], domain[0][1], plotres)
+	y = np.linspace(domain[1][0], domain[1][1], plotres)
 
+	X, Y = np.meshgrid(x, y)
+	#z = np.array( [func(a,b) for a, b in zip(np.ravel(X), np.ravel(Y))] )
+	z_approx = np.array( [approximator.evaluate((a,b)) for a, b in zip(np.ravel(X), np.ravel(Y))] )
+	#Z = z.reshape(X.shape)
+	Z_approx = z_approx.reshape(X.shape)
 
-x = np.linspace(domain['x'][0], domain['x'][1], 30)
-y = np.linspace(domain['y'][0], domain['y'][1], 30)
+	fig = plt.figure()
+	ax = Axes3D(fig)
+	#surf = ax.plot_surface(X, Y, Z, alpha=0.3, cmap=cm.winter, rstride=1, cstride=1, linewidth=0, antialiased=False)
+	surf = ax.plot_surface(X, Y, Z_approx, alpha=0.8, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, antialiased=False)
+	#ax.set_zlim(-4, 3)
+	ax.zaxis.set_major_locator(LinearLocator(10))
+	ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+	ax.set_xlabel('X')
+	ax.set_ylabel('Y')
+	ax.set_zlabel('Z')
+	print "...done ({} sec)".format(time.clock()-t0)
+	print "Displaying..."
+	plt.show()
+	print "Done."
 
-X, Y = np.meshgrid(x, y)
-z = np.array( [func(a,b) for a, b in zip(np.ravel(X), np.ravel(Y))] )
-z_approx = np.array( [approximator.evaluate((a,b)) for a, b in zip(np.ravel(X), np.ravel(Y))] )
-Z = z.reshape(X.shape)
-Z_approx = z_approx.reshape(X.shape)
+if __name__ == "__main__":
+	def func(x, y):
+		return (math.cos(2.5*math.pi*x) + math.sin(1.5*math.pi*y)) * (x+y)
 
-fig = plt.figure()
-ax = Axes3D(fig)
-#surf = ax.plot_surface(X, Y, Z, alpha=0.3, cmap=cm.winter, rstride=1, cstride=1, linewidth=0, antialiased=False)
-surf = ax.plot_surface(X, Y, Z_approx, alpha=0.8, cmap=cm.coolwarm, rstride=1, cstride=1, linewidth=0, antialiased=False)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+	domain = ((-1, 1),(-1, 1))
 
-plt.show()
+	plot3d(func, domain, gridsize=2)
